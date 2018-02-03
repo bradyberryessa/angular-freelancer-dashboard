af7b753402c74da9fc5373ca449692fd130061b0
Brady Berryessa <you@example.com>
2018-02-03 14:35:10 -0700

    Used observable for live data for the proposals

--- freelance-camp-fe/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/app/proposal/proposal-list.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
-@Component ({
+@Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html',
-  styleUrls: ['proposal-list-component.css']
+  providers: [ ProposalService ]
 })
-export class ProposalListComponent {
-  proposalOne: Proposal = new Proposal(15, "ABC Company", "http://bradyberryessa.com", "Angular 2", 150, 120, 15, "brady.berryessa@gmail.com")
-  proposalTwo: Proposal = new Proposal(99, "XYZ Company", "http://bradyberryessa.com", "Angular 2", 150, 120, 15, "brady.berryessa@gmail.com")
-  proposalThree: Proposal = new Proposal(300, "Something Company", "http://bradyberryessa.com", "Angular 2", 150, 120, 15, "brady.berryessa@gmail.com")
+export class ProposalListComponent implements OnInit {
+  proposals: Proposal[];
+  errorMessage: string;
+  mode = "Observable";
 
-  proposals: Proposal[] = [
-    this.proposalOne,
-    this.proposalTwo,
-    this.proposalThree
-  ]
+  constructor(
+    private proposalService: ProposalService
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getProposals());
+  }
+
+  getProposals() {
+    this.proposalService.getProposals()
+        .subscribe(
+          proposals => this.proposals = proposals,
+          error => this.errorMessage = <any>error
+        );
+  }
 }
+
+Proposal.create!(customer: "Google", portfolio_url: 'http://portfolio.jordanhudgens.com,' tools: 'Ruby on Rails, Angular 2, and Postgres' estimated_hours: 120, hourly_rate: 120, weeks_to_complete: 12, client_email: 'jordan@devcamp.com')
